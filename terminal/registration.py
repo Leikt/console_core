563f85072faf1a2a6ac3719c481093b8dc94b852
robin <leikt.solreihin@gmail.com>
2022-08-20 17:19:48 +0200

    Add command_set parameter

--- terminal/registration.py
+++ terminal/registration.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from .base_commands import CommandQuit
 from .exceptions import CommandRegistrationConflict
 from .variables import command_parser, commands
@@ -15,22 +17,26 @@ def register_plugin(module):
         register_command(command)
 
 
-def register_command(command: PCommand) -> bool:
+def register_command(command: PCommand, command_set: Dict[str, PCommand] = None) -> bool:
     """Register the given command."""
+    if command_set is None:
+        command_set = commands
     keyword = command.KEYWORD
     if keyword in commands:
         raise CommandRegistrationConflict(keyword)
 
     parser = command_parser.add_parser(command.KEYWORD)
     command.setup_parser(parser)
-    commands[keyword] = command
+    command_set[keyword] = command
     return True
 
 
-def unregister_command(keyword: str):
+def unregister_command(keyword: str, command_set: Dict[str, PCommand] = None):
     """Remove the command from the known ones. Warning: it does not reset the argument parser."""
+    if command_set is None:
+        command_set = commands
     if keyword in commands:
-        del commands[keyword]
+        del command_set[keyword]
 
 
 def reset():
